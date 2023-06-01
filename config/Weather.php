16eff63c86bd99f3e6fc6d19d25d532b635565de
Douglas Guilherme <douglas.guilherme0704@gmail.com>
2023-06-01 00:23:14 -0300

    troca de chave da api

--- config/Weather.php
+++ config/Weather.php
@@ -5,7 +5,7 @@ class Weather
 {
     public static function GetValues(float $lat, float $lon): \stdClass|string
     {
-        $apiKey = 'f1f2cd364462158087248953cd9d3a18';
+        $apiKey = 'sua chave da api';
         $url = "http://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&units=metric&lang=pt_br&exclude=current&appid=$apiKey";
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
